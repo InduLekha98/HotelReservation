b9f42e6a7f1f63760a5a7864012ff74b20114755
InduLekha98 <rindulekha09@gmail.com>
2021-03-08 22:55:53 +0530

    Addeed:Ability to find cheapest hotel for given range

--- HotelReservationTest1.java
+++ HotelReservationTest1.java
@@ -2,7 +2,6 @@ package com.hotel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HotelReservationTest1 {
@@ -15,4 +14,22 @@ public class HotelReservationTest1 {
         List hotelList = hotelReservationService.getHotels();
         Assertions.assertTrue(hotelList.contains(hotel));
     }
+
+    @Test
+    public void given3Hotels_whenInvokeFindCheapestHotel_shouldBeAbleToReturnCheapestHotel() {
+        HotelReservationService hotelReservationService = new HotelReservationService();
+        Hotel Lakewood = new Hotel("Lakewood",110);
+        Hotel Bridgewood = new Hotel("Bridgewood",160);
+        Hotel RidgeWood = new Hotel("Ridgewood",220);
+        hotelReservationService.addHotel(Lakewood);
+        hotelReservationService.addHotel(Bridgewood);
+        hotelReservationService.addHotel(RidgeWood);
+        int countNumberOfDays = hotelReservationService.countDays("2020-09-10","2020-09-12");
+        System.out.println(countNumberOfDays);
+        Hotel result = hotelReservationService.findCheapestHotel(countNumberOfDays);
+        System.out.println(result);
+        Assertions.assertTrue(hotelReservationService.HotelList.contains(result));
+    }
+
 }
+
