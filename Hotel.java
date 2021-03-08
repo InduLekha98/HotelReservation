ebb4ed991dab1678bb458205afeafadd86c59336
InduLekha98 <rindulekha09@gmail.com>
2021-03-08 23:15:03 +0530

    Addeed:Added weekday and weekend rates gor each hotel

--- Hotel.java
+++ Hotel.java
@@ -1,28 +1,56 @@
 package com.hotel;
 
+import java.util.Date;
+import java.util.Scanner;
+
 public class Hotel {
-        private String name;
-        private int rate;
+    public String hotelName;
+    public int weekDayRates;
+    public int weekEndRates;
+    public int rates;
+    public Scanner sc = new Scanner(System.in);
 
-        public Hotel(String name, int rate) {
-            this.name = name;
-            this.rate = rate;
-        }
-        public String toString() {
-            return "Hotel: "+ name + " Rate: " + rate;
-        }
-        public String getName() {
-            return name;
-        }
-        public void SetName(String name) {
-            this.name = name;
-        }
-        public void setRate(int count) {
-            this.rate = this.rate*count;
-        }
+    public Hotel(String hotelName, int weekDayRates, int weekEndRates) {
+        this.hotelName = hotelName;
+        this.weekDayRates = weekDayRates;
+    }
 
-    public int getRate() {
-        return rate;
+    public String getHotelName(){
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName){
+        this.hotelName = hotelName;
+    }
+
+    public int getWeekDayRates(){
+        return weekDayRates;
+    }
+
+    public void setWeekDayRates(int weekDayRates){
+        this.weekDayRates = weekDayRates;
+    }
+    public int getWeekEndRatesDayRates(){
+        return weekEndRates;
+    }
+
+    public void setWeekEndRatesDayRates(int weekEndRates){
+        this.weekEndRates = weekEndRates;
+    }
+
+    public int getRate(){
+        return rates;
+    }
+    public void setRate(int rates){
+        this.rates = rates;
+    }
+
+    @Override
+    public String toString() {
+        String hotel = "Hotel : " + this.getHotelName() +"\nWeekDayRate : " +this.getWeekDayRates();
+        return hotel;
+    }
+
+    public void add(String lakeWood, int i) {
     }
 }
-
