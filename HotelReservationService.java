ddd5efa694ada36da3385bb3f87a4aad3ac4c27f
InduLekha98 <rindulekha09@gmail.com>
2021-03-10 00:21:55 +0530

    Addeed:Ability to add ratings to each hotel

--- HotelReservationService.java
+++ HotelReservationService.java
@@ -1,17 +1,96 @@
 package com.hotel;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 
 public class HotelReservationService {
-    private List<Hotel> hotelList = new ArrayList<>();
-    int result;
+    public static Scanner sc = new Scanner(System.in);
+    private ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+    static long totalDays, totalWeekDays, totalWeekEndDays;
 
+    //Adding hotel.
     public void addHotel(Hotel hotel) {
         hotelList.add(hotel);
     }
 
-    public List<Hotel> getHotels() {
+    public void getHotelDetails() {
+        Hotel hotel1 = new Hotel("LakeWood", 110, 90, 3);
+        Hotel hotel2 = new Hotel("BridgeWood", 150, 50, 4);
+        Hotel hotel3 = new Hotel("RidgeWood", 220, 150, 5);
+        hotelList.add(hotel1);
+        hotelList.add(hotel2);
+        hotelList.add(hotel3);
+    }
+
+    public ArrayList<Hotel> getHotelList() {
         return hotelList;
+    }
+
+    public void setHotelList(ArrayList<Hotel> hotelList) {
+        this.hotelList = hotelList;
+    }
+
+    //To count number of days from the given range of date.
+    public long noOfWeekDays(String date1, String date2) {
+        LocalDate startDate = LocalDate.parse(date1);
+        LocalDate endDate = LocalDate.parse(date2);
+        DayOfWeek start = startDate.getDayOfWeek();
+        DayOfWeek end = endDate.getDayOfWeek();
+        totalDays = ChronoUnit.DAYS.between(startDate, endDate);
+        totalDays = totalDays + 1;
+        totalWeekEndDays = getTotalWeekEndDays(startDate, endDate);
+        totalWeekDays = totalDays - totalWeekEndDays;
+        System.out.println(totalWeekDays);
+        return totalWeekDays;
+    }
+
+    public long noOfWeekEnds(String date1, String date2) {
+        LocalDate startDate = LocalDate.parse(date1);
+        LocalDate endDate = LocalDate.parse(date2);
+        long weekEndDays = 0;
+        LocalDate next = startDate.minusDays(1);
+        //iterate from start date to end date
+        while ((next = next.plusDays(1)).isBefore(endDate.plusDays(1))) {
+            if (next.getDayOfWeek().toString().equals("SATURDAY") || next.getDayOfWeek().toString().equals("SUNDAY")) {
+                totalWeekEndDays++;
+            }
+        }
+        return (int) totalWeekEndDays;
+//        totalDays = ChronoUnit.DAYS.between(startDate, endDate);
+//        totalDays = totalDays + 1;
+//        totalWeekEndDays = getTotalWeekEndDays(startDate, endDate);
+//        totalWeekDays = totalDays - totalWeekEndDays;
+//        System.out.println(totalWeekDays);
+//        return totalWeekDays;
+    }
+
+    public int getTotalWeekEndDays(LocalDate start, LocalDate end) {
+        long weekEndDays = 0;
+        LocalDate next = start.minusDays(1);
+        //iterate from start date to end date
+        long totalWeekEndDays = 0;
+        while ((next = next.plusDays(1)).isBefore(end.plusDays(1))) {
+            if (next.getDayOfWeek().toString().equals("SATURDAY") || next.getDayOfWeek().toString().equals("SUNDAY")) {
+                totalWeekEndDays++;
+            }
+        }
+        return (int) totalWeekEndDays;
+    }
+
+    //To get the cheapest hotel.
+    public Hotel getCheapestHotel(String date1, String date2) {
+        totalWeekDays = noOfWeekDays(date1, date2);
+        totalWeekEndDays = noOfWeekEnds(date1, date2);
+        hotelList.stream().map(r -> {
+            r.setRate(totalWeekDays, totalWeekEndDays);
+            return r.getRate();
+        }).collect(Collectors.toList());
+        Hotel minimumRate = hotelList.stream().min(Comparator.comparing(Hotel::getWeekDayRates)).orElseThrow(NoSuchElementException::new);
+        return minimumRate;
     }
 }
