2fae6df258e2fd5e84d9cdb060d44e23c49101bf
InduLekha98 <rindulekha09@gmail.com>
2021-03-10 19:05:19 +0530

    Addeed:Ability to find cheapest best rated hotel for reward customer

--- HotelReservationTest1.java
+++ HotelReservationTest1.java
@@ -8,7 +8,16 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class HotelReservationTest1<result> {
+    HotelReservationService hotelReservationService = new HotelReservationService();
+    public void addHotelData(){
 
+        Hotel Lakewood = new Hotel("Lakewood",110,90,80,80,3);
+        Hotel Bridgewood = new Hotel("Bridgewood",150,50,110,50,4);
+        Hotel RidgeWood = new Hotel("Ridgewood",220,150,100,40,5);
+        hotelReservationService.addHotel(Lakewood);
+        hotelReservationService.addHotel(Bridgewood);
+        hotelReservationService.addHotel(RidgeWood);
+    }
     @Test
     public void givenHotel_ShouldReturnTrue() {
         HotelReservationService hotelReservationService = new HotelReservationService();
@@ -21,13 +30,7 @@ public class HotelReservationTest1<result> {
 
     @Test
     public void givenHotels_shouldReturnCheapestHotel(){
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotel Lakewood = new Hotel("Lakewood",110,90,80,80,3);
-        Hotel Bridgewood = new Hotel("Bridgewood",150,50,110,50,4);
-        Hotel RidgeWood = new Hotel("Ridgewood",220,150,100,40,5);
-        hotelReservationService.addHotel(Lakewood);
-        hotelReservationService.addHotel(Bridgewood);
-        hotelReservationService.addHotel(RidgeWood);
+        addHotelData();
         List hotelList = hotelReservationService.getHotelList();
         System.out.println(hotelList);
 //          long days = hotelReservation.noOfWeekEnds("10-09-2021","12-09-2021");
@@ -37,13 +40,7 @@ public class HotelReservationTest1<result> {
 
     @Test
     public void givenDateRange_shouldReturnCheapestHotel(){
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotel Lakewood = new Hotel("Lakewood",110,90,80,80,3);
-        Hotel Bridgewood = new Hotel("Bridgewood",150,50,110,50,4);
-        Hotel RidgeWood = new Hotel("Ridgewood",220,150,100,40,5);
-        hotelReservationService.addHotel(Lakewood);
-        hotelReservationService.addHotel(Bridgewood);
-        hotelReservationService.addHotel(RidgeWood);
+        addHotelData();
         List hotelList = hotelReservationService.getHotelList();
         System.out.println(hotelList);
         Hotel result = hotelReservationService.getCheapestHotel("2020-09-10","2020-09-12");
@@ -52,13 +49,7 @@ public class HotelReservationTest1<result> {
 
     @Test
     public void givenDateRange_basisOfWeekDayWeekEnd_shouldReturnCheapestHotel(){
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotel Lakewood = new Hotel("Lakewood",110,90,80,80,3);
-        Hotel Bridgewood = new Hotel("Bridgewood",150,50,110,50,4);
-        Hotel RidgeWood = new Hotel("Ridgewood",220,150,100,40,5);
-        hotelReservationService.addHotel(Lakewood);
-        hotelReservationService.addHotel(Bridgewood);
-        hotelReservationService.addHotel(RidgeWood);
+        addHotelData();
         List hotelList = hotelReservationService.getHotelList();
         Hotel result = hotelReservationService.getCheapestHotel("2020-09-11","2020-09-12");
         Assertions.assertTrue(hotelList.contains(result));
@@ -66,13 +57,7 @@ public class HotelReservationTest1<result> {
 
     @Test
     public void givenDates_basisOfWeekDayWeekEnd_shouldReturnCheapestHotel(){
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotel Lakewood = new Hotel("Lakewood",110,90,80,80,3);
-        Hotel Bridgewood = new Hotel("Bridgewood",150,50,110,50,4);
-        Hotel RidgeWood = new Hotel("Ridgewood",220,150,100,40,5);
-        hotelReservationService.addHotel(Lakewood);
-        hotelReservationService.addHotel(Bridgewood);
-        hotelReservationService.addHotel(RidgeWood);
+        addHotelData();
         List hotelList = hotelReservationService.getHotelList();
         Hotel result = hotelReservationService.getCheapestHotel("2020-09-11","2020-09-12");
         Assertions.assertTrue(hotelList.contains(result));
@@ -80,13 +65,7 @@ public class HotelReservationTest1<result> {
 
     @Test
     public void givenDates_basisOfWeekDayWeekEnd_shouldReturnBestCheapestHotel(){
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotel Lakewood = new Hotel("Lakewood",110,90,80,80,3);
-        Hotel Bridgewood = new Hotel("Bridgewood",150,50,110,50,4);
-        Hotel RidgeWood = new Hotel("Ridgewood",220,150,100,40,5);
-        hotelReservationService.addHotel(Lakewood);
-        hotelReservationService.addHotel(Bridgewood);
-        hotelReservationService.addHotel(RidgeWood);
+        addHotelData();
         List hotelList = hotelReservationService.getHotelList();
         Hotel result = hotelReservationService.getBestCheapHotel("2020-09-11","2020-09-12");
         Assertions.assertTrue(hotelList.contains(result));
@@ -94,30 +73,25 @@ public class HotelReservationTest1<result> {
 
     @Test
     public void givenDates_basisOfWeekDayWeekEnd_shouldReturnBestHotel(){
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotel Lakewood = new Hotel("Lakewood",110,90,80,80,3);
-        Hotel Bridgewood = new Hotel("Bridgewood",150,50,110,50,4);
-        Hotel RidgeWood = new Hotel("Ridgewood",220,150,100,40,5);
-        hotelReservationService.addHotel(Lakewood);
-        hotelReservationService.addHotel(Bridgewood);
-        hotelReservationService.addHotel(RidgeWood);
+        addHotelData();
         List hotelList = hotelReservationService.getHotelList();
         Hotel result = hotelReservationService.getBestRatedHotel("2020-09-11","2020-09-12");
         Assertions.assertTrue(hotelList.contains(result));
     }
 
-
     @Test
     public void addSpecialRates_reward(){
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotel Lakewood = new Hotel("Lakewood",110,90,80,80,3);
-        Hotel Bridgewood = new Hotel("Bridgewood",150,50,110,50,4);
-        Hotel RidgeWood = new Hotel("Ridgewood",220,150,100,40,5);
-        hotelReservationService.addHotel(Lakewood);
-        hotelReservationService.addHotel(Bridgewood);
-        hotelReservationService.addHotel(RidgeWood);
+        addHotelData();
         List hotelList = hotelReservationService.getHotelList();
         Hotel result = hotelReservationService.getBestRatedHotel("2020-09-11","2020-09-12");
+        Assertions.assertTrue(hotelList.contains(result));
+    }
+
+    @Test
+    public void givenDate_customerType_ShouldReturnCheapestHotel(){
+        addHotelData();
+        List hotelList = hotelReservationService.getHotelList();
+        Hotel result = hotelReservationService.addHotel_CustomerType("2020-09-11","2020-09-12","Reward");
         Assertions.assertTrue(hotelList.contains(result));
     }
 
